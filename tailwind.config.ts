ba269b17dba0c133b3388e51097de42056663a40
jeanchongdev <grarenajeanpro@gmail.com>
2025-07-20 12:25:03 -0500

    Arreglo 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./**/*.{js,ts,jsx,tsx,mdx}",
     "*.{js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
